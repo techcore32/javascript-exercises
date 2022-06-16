622878b5c3081ae76169bb3fc7ea99b96df00857
DiegoRT32 <sorpristerton43@proton.me>
2022-06-16 17:56:39 -0500

    Solve exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,21 @@
-const repeatString = function() {
+const repeatString = (str,num) => {
 
+    let repeatString = "";
+   
+    if(num >= 0){
+        while(num > 0) {
+            repeatString += str;
+            num--;
+        }   
+    }
+
+    else{
+        return "ERROR";
+    }
+
+    return repeatString;
 };
 
+repeatString ("",-1);
 // Do not edit below this line
 module.exports = repeatString;
