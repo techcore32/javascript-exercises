18a63136314abf03e62a98e1ca72945fbe4e171e
DiegoRT32 <sorpristerton43@proton.me>
2022-06-18 00:51:16 -0500

    Solve exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,22 @@
-const sumAll = function() {
+const sumAll = (...sum) => {
+    
+    let result = 0;
 
-};
+    sum.sort((a,b) => a-b);
 
+ if(!Number.isInteger(sum[1]) || !Number.isInteger(sum[0])) return "ERROR"; 
+    
+    if(sum[0] < 0 || sum[1] < 0) return "ERROR";
+
+        while(sum[0] <= sum[1]){
+        result += sum[0];
+        sum[0]++
+		}
+    
+
+  return result;
+}
+
+  sumAll(10, [90, 1]);
 // Do not edit below this line
 module.exports = sumAll;
