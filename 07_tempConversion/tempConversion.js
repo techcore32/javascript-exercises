73d1066375cf261c2b797ff316458d21ca9947d8
DiegoRT32 <sorpristerton43@proton.me>
2022-06-18 16:59:45 -0500

    Solve exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,17 @@
-const ftoc = function() {
-
-};
-
-const ctof = function() {
-
-};
+const ftoc = function(temp) {
+  let conversion = (temp - 32) * (5/9);
+   let result = Math.round(conversion* 10)/10;
+   return result;
+ };
+ 
+ const ctof = function(temp) {
+ let conversion = temp * (9/5)+32;
+ let result = Math.round(conversion* 10)/10;
+   return result;
+ };
+ 
+ ftoc(-100);
+ ctof(-10);
 
 // Do not edit below this line
 module.exports = {
