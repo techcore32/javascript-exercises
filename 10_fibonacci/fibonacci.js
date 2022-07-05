12e77c4f0d1f2dbe5013b4f6dd8bb3449998312a
DiegoRT32 <sorpristerton43@proton.me>
2022-07-05 18:06:11 -0500

    Solve exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
 
-};
+    let limit = parseInt(n);
 
+    if(limit < 0) return "OOPS";
+    num1 = 1;
+    num2 = 0;
+     
+      for(let i = 1; i <= limit; i++){
+        let sum = num1 + num2;
+        num1 = num2
+        num2 = sum;
+      }
+    
+   return num2;
+  };
+  
+  fibonacci("8");
 // Do not edit below this line
 module.exports = fibonacci;
