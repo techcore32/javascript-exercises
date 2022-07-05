cf47dbeb2142b4920b851817e7b5f5d737485821
DiegoRT32 <sorpristerton43@proton.me>
2022-07-05 16:38:33 -0500

    Solve Exercise 8 and 9

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,27 +1,43 @@
-const add = function() {
-	
+const add = function(a,b) {
+  return a+b;
+  };
+  
+  add(2,6);
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const subtract = function() {
-	
-};
+  subtract(10,4)
 
-const sum = function() {
-	
-};
 
-const multiply = function() {
+const sum = function(a) {
+  
+  return a.reduce((total, operator) => total + operator, 0);
 
 };
+  
+sum([]);
 
-const power = function() {
-	
+const multiply = function(a) {
+  return a.reduce((total, operator) => total * operator, 1);
 };
 
-const factorial = function() {
-	
+multiply([2,4,6,8,10,12,14]);
+
+const power = function(a,b) {
+	return Math.pow(a,b);
 };
 
+power(4,3);
+
+const factorial = function(a) {
+  if (a === 0) {
+    return 1;
+  }
+  return a * factorial(a-1);
+};
+
+factorial(10);
 // Do not edit below this line
 module.exports = {
   add,
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,17 @@
-const palindromes = function () {
+const palindromes = function (text) {
 
-};
+    const regex = /[^A-Za-z0-9]/g;
+    text = text.toLowerCase().replace(regex, '');
+    
+    let len = text.length;
 
+    for (let i = 0; i < len/2; i++) {
+       if (text[i] !== text[len - 1 - i]) {
+          return false;
+       };
+    };
+    return true;
+ };
+ palindromes("ZZZZ car, a man, a maracaz.")
 // Do not edit below this line
 module.exports = palindromes;
