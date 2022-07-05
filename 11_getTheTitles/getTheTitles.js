bba79694ad625b830d7760c83e0dcdee44f69e77
DiegoRT32 <sorpristerton43@proton.me>
2022-07-05 18:18:05 -0500

    Solve exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,17 @@
-const getTheTitles = function() {
-
+const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+  ]
+const getTheTitles = function(a) {
+return a.map(books =>  books.title);
 };
 
+getTheTitles(books)
 // Do not edit below this line
 module.exports = getTheTitles;
